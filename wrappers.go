c52e07463c8c8fdd77d8d1de65a53138af3805bf
Lord_Of_Death::Osiris <66261812+Osiris9999@users.noreply.github.com>
2021-11-26 13:09:14 +0530

    Update wrappers.go

--- wrappers.go
+++ wrappers.go
@@ -15,21 +15,3 @@ func F(str string, arg ...interface{}) string {
 func f(s string, arg ...interface{}) string {
 	return fmt.Sprintf(s, arg...)
 }
-
-// StrToInt converts a string into an integer.
-func StrToInt(string_integer string) int {
-	// i, _ := strconv.ParseInt(string_integer, 10, 32)
-	i, _ := strconv.Atoi(string_integer)
-	return i
-}
-
-// IntToStr converts an integer into a string.
-func IntToStr(i int) string {
-	return strconv.Itoa(i)
-}
-
-// RandomInt returns an integer within a given range.
-func RandomInt(min int, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min) + min
-}
